7c91c8d3294800242c342064c132e90d9f84183e
logicalmechanism <logical.mechanism@protonmail.com>
2024-07-15 14:54:20 -0700

    adding in a moderation data structure for a cogno

--- headless/00_createScriptReferences.sh
+++ headless/00_createScriptReferences.sh
@@ -10,13 +10,13 @@ ${cli} query protocol-parameters ${network} --out-file ./tmp/protocol.json
 # Addresses
 reference_address=$(cat ./wallets/reference-wallet/payment.addr)
 
-# perma lock the script references to the reference contract
-reference_script_path="../contracts/reference_contract.plutus"
-script_reference_address=$(${cli} address build --payment-script-file ${reference_script_path} ${network})
-
 # address to send the leftover ada too
 change_address=$(jq -r '.change_address' ../config.json)
 
+# perma lock the script references to the reference contract
+reference_script_path="../contracts/reference_contract.plutus"
+# script_reference_address=$(${cli} address build --payment-script-file ${reference_script_path} ${network})
+script_reference_address=${change_address}
 
 echo -e "\033[0;35m\nGathering UTxO Information  \033[0m"
 ${cli} query utxo \
