49750903d7a776198d048e081a6909f6feec1b70
logicalmechanism <logical.mechanism@protonmail.com>
2024-07-17 17:24:04 -0700

    pushing up and tagging out a alpha release

--- complete_build.sh
+++ complete_build.sh
@@ -17,10 +17,10 @@ echo -e "\033[1;34m Building Contracts \033[0m"
 # aiken build --trace-level silent --filter-traces user-defined
 
 # partial traces
-aiken build --trace-level compact --filter-traces user-defined
+# aiken build --trace-level compact --filter-traces user-defined
 
 # keep the traces for testing
-# aiken build --trace-level verbose --filter-traces all
+aiken build --trace-level verbose --filter-traces all
 
 ran=$(jq -r '.random_string' config.json)
 ran_cbor=$(python3 -c "import cbor2;hex_string='${ran}';data = bytes.fromhex(hex_string);encoded = cbor2.dumps(data);print(encoded.hex())")
