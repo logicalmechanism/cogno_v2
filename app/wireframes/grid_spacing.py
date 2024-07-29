485dd4fd7ad1af99e87e54b791ba0e966c099b2d
logicalmechanism <logical.mechanism@protonmail.com>
2024-07-29 16:35:18 -0700

    penpot wireframes for alpha

--- app/wireframes/grid_spacing.py
+++ app/wireframes/grid_spacing.py
@@ -1,6 +1,10 @@
+import argparse
+
+
 def allowed_values(n: int, z: int) -> None:
     # n*x + (n+1)*y = z
-    print(f"Find all solutions for {n} elements to fit inside {z} with {n + 1} spaces between:")
+    print(
+        f"Find all solutions for {n} elements to fit inside {z} with {n + 1} spaces between:")
     for x in range(1, z):
         for y in range(1, z):
             if n * x + (n + 1) * y == z:
@@ -8,6 +12,10 @@ def allowed_values(n: int, z: int) -> None:
 
 
 if __name__ == "__main__":
-    n = 10
-    z = 800
-    allowed_values(n, z)
+    parser = argparse.ArgumentParser(description='Calculate grid spacing.')
+    parser.add_argument('n', type=int, help='Number of elements')
+    parser.add_argument('z', type=int, help='Total space size')
+
+    args = parser.parse_args()
+
+    allowed_values(args.n, args.z)
