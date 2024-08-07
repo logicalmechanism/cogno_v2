88057ff5fe745240dc82749be15d18d4162b3962
logicalmechanism <logical.mechanism@protonmail.com>
2024-08-06 17:38:57 -0700

    added my threads and top threads filter; search is now a filter

--- app/components/Thread/filters.ts
+++ app/components/Thread/filters.ts
@@ -15,13 +15,6 @@ export const createFilterFunctions = (
   threads: UTxO[]
 ) => {
 
-  // show all threads
-  const handleFilterAll = () => {
-    setFilteredThreads(threads);
-    setCurrentPage(1);
-    setSearchInput('');
-  };
-
   // show threads from a specific cogno
   const handleFilterMyThreads = () => {
     const cognoTokenName = sessionStorage.getItem('cognoTokenName');
@@ -35,6 +28,7 @@ export const createFilterFunctions = (
     setSearchInput('');
   };
 
+  // filter by the selected category, all is everything
   const handleFilterByCategory = (category: string) => {
     let filtered;
     if (category === "All") {
@@ -50,9 +44,82 @@ export const createFilterFunctions = (
     setSearchInput('');
   };
 
+  // show threads from the my friend list
+  const handleFilterMyFriends = () => {
+    const storedFriendList = JSON.parse(sessionStorage.getItem('friendList') || '[]');
+    const filtered = threads.filter(thread => {
+      const parsedDatum = parseDatumCbor(thread.output.plutusData!);
+      return storedFriendList.some((friend: string) => friend === parsedDatum.fields[5].bytes);
+    });
+    setFilteredThreads(filtered);
+    setCurrentPage(1);
+    setSearchInput('');
+  };
+
+  // show all threads but ordered by most comments
+  const handleFilterTopThreads = () => {
+    console.log('filter top threasd');
+    
+    const filtered = [...threads].sort((a, b) => {
+      const parsedDatumA = parseDatumCbor(a.output.plutusData!);
+      const parsedDatumB = parseDatumCbor(b.output.plutusData!);
+      
+      const lengthA = parsedDatumA.fields[4].list.length;
+      const lengthB = parsedDatumB.fields[4].list.length;
+  
+      // Sort in descending order (largest lengths first)
+      return lengthA - lengthB;
+    });
+    setFilteredThreads(filtered);
+    setCurrentPage(1);
+    setSearchInput('');
+  };
+
+  const handleFilterBySearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setSearchInput(value);
+
+    // return to all if search is now empty string
+    if (value.trim() === '') {
+      setFilteredThreads(threads);
+      setCurrentPage(1);
+      return;
+    }
+
+    const filtered = threads.filter(thread => {
+      // field 3 is category which already has a filtered
+      // field 4 is the comment list which wont be filtered
+      //
+      // get the parsed thread datum
+      const parsedDatum = parseDatumCbor(thread.output.plutusData!);
+      // title
+      const field0 = hexToString(parsedDatum.fields[0].bytes).toLowerCase();
+      // content
+      const field1 = hexToString(parsedDatum.fields[1].bytes).toLowerCase();
+      // image url
+      const field2 = hexToString(parsedDatum.fields[2].bytes).toLowerCase();
+      // cogno tkn
+      const field5 = parsedDatum.fields[5].bytes.toLowerCase();
+      // make everything lowercase
+      const searchValue = value.toLowerCase();
+      console.log(field2, searchValue);
+      //
+      // find it somewhere
+      return field0.includes(searchValue) || 
+             field1.includes(searchValue) || 
+             field2.includes(searchValue) || 
+             field5.includes(searchValue);
+    });
+
+    setFilteredThreads(filtered);
+    setCurrentPage(1);
+  };
+
   return {
-    handleFilterAll,
     handleFilterMyThreads,
-    handleFilterByCategory
+    handleFilterByCategory,
+    handleFilterMyFriends,
+    handleFilterTopThreads,
+    handleFilterBySearch,
   };
 };
